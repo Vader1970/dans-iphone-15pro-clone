abeb37eb24027720e0e0f3393e564ba1dc0929a1
Vader1970 <daniel.wilkey@gmail.com>
2024-09-19 18:42:37 +1200

    Second push

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://487ea555fb2832e8c2dca4bed285eb08@o4507959366778880.ingest.de.sentry.io/4507960638308432",
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
